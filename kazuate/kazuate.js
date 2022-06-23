821f46e0551e2d9a44e96ebe2c83adfdab8b4c23
G184062021 <218406@st.takushoku-u.ac.jp>
2022-06-23 15:34:01 +0900

    2022/6/23

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -1,6 +1,6 @@
 // 答え
 let kotae = Math.floor(Math.random()*10) + 1;
-console.log('答え: ' + kotae);      // デバッグ用
+console.log('答え(デバッグ用): ' + kotae);      // デバッグ用
 
 // 入力回数（予想回数）
 let kaisu = 0;
@@ -15,14 +15,18 @@ hantei();
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
+    kaisu = kaisu + 1;
     console.log(kaisu + '回目の予想：'+yoso);
-    if (yoso > 4) {
+    if (kaisu >= 4) {
         console.log('答えは'+kotae+'でした.すでにゲームは終わっています');
     } else if(yoso === kotae) {
         console.log('正解です.おめでとう！');
+        kaisu = 4;
+    } else if(kaisu === 3) {
+        console.log('まちがい．残念でした答えは '+kotae+' です．');
     } else if(kotae > yoso) {
         console.log('まちがい．答えはもっと大きいですよ');
-    } else if(kotae > yoso) {
+    } else if(kotae < yoso) {
         console.log('まちがい．答えはもっと小さいですよ');
     }
 
